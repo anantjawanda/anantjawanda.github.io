9dc0744c11fe127b237889558ddb3a5d8b51964f
anantjawanda <anant.jawanda@ryerson.ca>
2020-07-27 19:00:47 -0400

    Updates

---
+++ precache-manifest.6c7c885d99223fc41ced904b481e11e4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ae2096a9ee980a1b6ed8f5c1a7c6c1",
+    "url": "/anantjawanda.github.io/index.html"
+  },
+  {
+    "revision": "6b1be65ac851cff63c0d",
+    "url": "/anantjawanda.github.io/static/css/main.3bde2799.chunk.css"
+  },
+  {
+    "revision": "bd6058c19be2f5b0fb5a",
+    "url": "/anantjawanda.github.io/static/js/2.49c0e482.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anantjawanda.github.io/static/js/2.49c0e482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1be65ac851cff63c0d",
+    "url": "/anantjawanda.github.io/static/js/main.a9ceeec6.chunk.js"
+  },
+  {
+    "revision": "c776f06204fcfa93c152",
+    "url": "/anantjawanda.github.io/static/js/runtime-main.ba0610d8.js"
+  },
+  {
+    "revision": "dac76b0b332b565f12d183cb10c13343",
+    "url": "/anantjawanda.github.io/static/media/Regatto.dac76b0b.woff"
+  },
+  {
+    "revision": "fb58f81bbfbe7f3b3c3c795aab881fc8",
+    "url": "/anantjawanda.github.io/static/media/github-button.fb58f81b.svg"
+  },
+  {
+    "revision": "3ca53cc8ab951c8271b82aa75b455873",
+    "url": "/anantjawanda.github.io/static/media/laptop.3ca53cc8.svg"
+  },
+  {
+    "revision": "6c0a07fb4333d5341b02c9b1fb842af2",
+    "url": "/anantjawanda.github.io/static/media/linkedin-button.6c0a07fb.svg"
+  },
+  {
+    "revision": "012a2b34babb0bc1644f75e8afb356e1",
+    "url": "/anantjawanda.github.io/static/media/mail-btn.012a2b34.svg"
+  },
+  {
+    "revision": "e39cfde898f8ade1c7dacdd0d63452d8",
+    "url": "/anantjawanda.github.io/static/media/shadow.e39cfde8.svg"
+  },
+  {
+    "revision": "290c037ce54fb2f7f3741a714e2db775",
+    "url": "/anantjawanda.github.io/static/media/view-project-btn.290c037c.svg"
+  },
+  {
+    "revision": "f7c40dab3f1f7bcbc2d6ed121748820e",
+    "url": "/anantjawanda.github.io/static/media/viewsite-btn.f7c40dab.svg"
+  }
+]);
